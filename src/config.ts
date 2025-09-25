902f1bcd870d983aa5899fa3f13b1167554a4372
GoodNightIsabelle <gnisabel@outlook.com>
2025-09-25 15:20:07 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -15,19 +15,25 @@ export const siteConfig = {
     {
       title: "2025",
       bullets: [
-        "Q. Qin, R. Aghili, H. Li, and E. Merlo, “Preprocessing is All You Need: Boosting the Performance of Log Parsers With a General Preprocessing Framework,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER) (accepted), 2025. [Link](https://arxiv.org/abs/2412.05254).",
-        "G. Caumartin, Q. Qin, H. Li, and D. Costa, “Exploring the Potential of Llama Models in Automated Code Refinement: A Replication Study,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER) (accepted), 2025. [Link](https://arxiv.org/abs/2412.02789).",
-        "Q. Qin, H. Li, E. Merlo, and M. Lamothe, “Automated, Unsupervised, and Auto-parameterized Inference of Data Patterns and Anomaly Detection,” in Proceedings of the 47th International Conference on Software Engineering (ICSE) (accepted), 2025. [Link](https://arxiv.org/abs/2412.05240).",
-        "Q. Qin, B. Djian, E. Merlo, H. Li, S. Gambs, “Representation-Based Fairness Evaluation and Bias Correction Robustness Assessment in Neural Networks,” Information and Software Technology (IST) (accepted), vol 188, 2025. [Link](https://www.sciencedirect.com/science/article/pii/S0950584925002150).",
-        "Q. Qin and E. Merlo, “Prune Bias From the Root: Bias Removal and Fairness Estimation by Pruning Sensitive Attributes in Pre-trained DNN Models,” Information and Software Technology (IST) (accepted), vol 188, 2025. "
-      ],
+        {text: "Q. Qin and E. Merlo, “Prune Bias From the Root: Bias Removal and Fairness Estimation by Pruning Sensitive Attributes in Pre-trained DNN Models,” Information and Software Technology (IST) (accepted), vol 188, 2025. ", },
+        {text: "Representation-Based Fairness Evaluation and Bias Correction Robustness Assessment in Neural Networks,” Information and Software Technology (IST) (accepted), vol 188, 2025.", 
+         link: "https://www.sciencedirect.com/science/article/pii/S0950584925002150" },
+        {text: "Q. Qin, H. Li, E. Merlo, and M. Lamothe, “Automated, Unsupervised, and Auto-parameterized Inference of Data Patterns and Anomaly Detection,” in Proceedings of the 47th International Conference on Software Engineering (ICSE) (accepted), 2025.", 
+         link: "https://arxiv.org/abs/2412.05240" },
+        {text: "Q. Qin, R. Aghili, H. Li, and E. Merlo, “Preprocessing is All You Need: Boosting the Performance of Log Parsers With a General Preprocessing Framework,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER) (accepted), 2025.", 
+         link: "https://arxiv.org/abs/2412.05254" },
+        {text: "G. Caumartin, Q. Qin, H. Li, and D. Costa, “Exploring the Potential of Llama Models in Automated Code Refinement: A Replication Study,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER) (accepted), 2025.", 
+         link: "https://arxiv.org/abs/2412.02789" },
+        ],
     },
     {
       title: "2024",
       bullets: [
-        "Q. Qin, H. Li, E. Merlo, “Wrangling Data Issues to be Wran- gled: Literature Review, Taxonomy, and Industry Case Study,” arXiv preprint, 2024.",
-        "R. Aghili, Q. Qin, H. Li, and F. Khomh, “Understanding Web Application Workloads and Their Applications: Systematic Literature Review and Characterization,” in Proceedings of the 40th IEEE International Conference on Software Maintenance and Evolution (ICSME) (accepted), 2024.",
-      ],
+        {text: "R. Aghili, Q. Qin, H. Li, and F. Khomh, “Understanding Web Application Workloads and Their Applications: Systematic Literature Review and Characterization,” in Proceedings of the 40th IEEE International Conference on Software Maintenance and Evolution (ICSME) (accepted), 2024, pp. 474–486. ", 
+         link: "https://arxiv.org/abs/2405.1603" },
+        {text: "Q. Qin, H. Li, E. Merlo, “Wrangling Data Issues to be Wran- gled: Literature Review, Taxonomy, and Industry Case Study,” arXiv preprint, 2024.", 
+         link: "https://ieeexplore.ieee.org/abstract/document/10795077" },
+        ],
     },
   ],
   projects: [
@@ -40,29 +46,33 @@ export const siteConfig = {
     },
     {
       name: "Program Committee Member",
-      bullet: ["The International Conference on Evaluation and Assessment in Software Engineering (EASE), Committee Member in AI Models / Data track, 2025",
-               "The International Conference on Collaborative Advances in Software and Computing (CASCON), Committee Member in Artifacts track, 2025",
-               "The International Conference on Technical Debt (TechDebt), Committee Member in Technical Papers track, 2026",
-               "The International Workshop on Fairness in Software Systems (Fairness), Journal-First Co-Chair, 2026"
-              ],
+      bullets: [
+        { text: "The International Conference on Evaluation and Assessment in Software Engineering (EASE), Committee Member in AI Models / Data track, 2025"},
+        { text: "The International Conference on Collaborative Advances in Software and Computing (CASCON), Committee Member in Artifacts track, 2025" },
+        { text: "The International Conference on Technical Debt (TechDebt), Committee Member in Technical Papers track, 2026" },
+        { text: "The International Workshop on Fairness in Software Systems (Fairness), Journal-First Co-Chair, 2026" },
+      ],
     },
     {
       name: "Journal Reviewer",
-      bullet: ["IEEE Transactions on Software Engineering (TSE)",
-               "Information and Software Technology (IST)",
-              ],
+      bullets: [
+        { text: "IEEE Transactions on Software Engineering (TSE)"},
+        { text: "Information and Software Technology (IST)" }
+      ]
     },
     {
       name: "Volunteering",
-      bullet: ["Software Engineering for Machine Learning Applications Symposium (SEMLA), 2024",
-               "The IEEE/ACM International Conference on Software Engineering (ICSE), 2025",
-              ],
+      bullets: [
+        { text: "Software Engineering for Machine Learning Applications Symposium (SEMLA), 2024"},
+        { text: "The IEEE/ACM International Conference on Software Engineering (ICSE), 2025" }
+      ]
     },
     {
       name: "Industrial Experience",
-      bullet: ["Air Canada, Data Analyst Intern (Sep. 2023 – Aug. 2024)",
-               "Intel Joint Research Institute on Intelligent Edge Computing, Computer Vision Algorithm Engineer Intern (Aug. 2022 – Apr. 2023)",
-              ],
+      bullets: [
+        { text: "Air Canada, Data Analyst Intern (Sep. 2023 – Aug. 2024)"},
+        { text: "Intel Joint Research Institute on Intelligent Edge Computing, Computer Vision Algorithm Engineer Intern (Aug. 2022 – Apr. 2023)" }
+      ]
     },
   ],
   education: [
