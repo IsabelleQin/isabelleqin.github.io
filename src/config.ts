d7ad06c76cdbfcfc333825901ca36fc2e603299b
GoodNightIsabelle <gnisabel@outlook.com>
2025-11-25 15:18:16 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -18,22 +18,22 @@ export const siteConfig = {
     {
       title: "2025",
       bullets: [
-        {text: "Q. Qin and E. Merlo, “Prune Bias From the Root: Bias Removal and Fairness Estimation by Pruning Sensitive Attributes in Pre-trained DNN Models,” Information and Software Technology (IST) (accepted), vol 188, 2025. ", 
+        {text: "Q. Qin and E. Merlo, “Prune Bias From the Root: Bias Removal and Fairness Estimation by Pruning Sensitive Attributes in Pre-trained DNN Models,” Information and Software Technology (IST), vol 188, 2025. ", 
          link: "https://www.sciencedirect.com/science/article/pii/S0950584925002459"},
-        {text: "Q. Qin, B. Dijan, E. Merlo, H. Li, and S. Gambs, Representation-Based Fairness Evaluation and Bias Correction Robustness Assessment in Neural Networks,” Information and Software Technology (IST) (accepted), vol 188, 2025.", 
+        {text: "Q. Qin, B. Dijan, E. Merlo, H. Li, and S. Gambs, Representation-Based Fairness Evaluation and Bias Correction Robustness Assessment in Neural Networks,” Information and Software Technology (IST), vol 188, 2025.", 
          link: "https://www.sciencedirect.com/science/article/pii/S0950584925002150" },
-        {text: "Q. Qin, H. Li, E. Merlo, and M. Lamothe, “Automated, Unsupervised, and Auto-parameterized Inference of Data Patterns and Anomaly Detection,” in Proceedings of the 47th International Conference on Software Engineering (ICSE) (accepted), 2025.", 
+        {text: "Q. Qin, H. Li, E. Merlo, and M. Lamothe, “Automated, Unsupervised, and Auto-parameterized Inference of Data Patterns and Anomaly Detection,” in Proceedings of the 47th International Conference on Software Engineering (ICSE), 2025.", 
          link: "https://arxiv.org/abs/2412.05240" },
-        {text: "Q. Qin, R. Aghili, H. Li, and E. Merlo, “Preprocessing is All You Need: Boosting the Performance of Log Parsers With a General Preprocessing Framework,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER) (accepted), 2025.", 
+        {text: "Q. Qin, R. Aghili, H. Li, and E. Merlo, “Preprocessing is All You Need: Boosting the Performance of Log Parsers With a General Preprocessing Framework,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER), 2025.", 
          link: "https://arxiv.org/abs/2412.05254" },
-        {text: "G. Caumartin, Q. Qin, S. Chatragadda, J. Panjrolia, H. Li, and D. Costa, “Exploring the Potential of Llama Models in Automated Code Refinement: A Replication Study,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER) (accepted), 2025.", 
+        {text: "G. Caumartin, Q. Qin, S. Chatragadda, J. Panjrolia, H. Li, and D. Costa, “Exploring the Potential of Llama Models in Automated Code Refinement: A Replication Study,” in Proceedings of the 32nd IEEE International Conference on Software Analysis, Evolution and Reengineering (SANER), 2025.", 
          link: "https://arxiv.org/abs/2412.02789" },
         ],
     },
     {
       title: "2024",
       bullets: [
-        {text: "R. Aghili, Q. Qin, H. Li, and F. Khomh, “Understanding Web Application Workloads and Their Applications: Systematic Literature Review and Characterization,” in Proceedings of the 40th IEEE International Conference on Software Maintenance and Evolution (ICSME) (accepted), 2024, pp. 474–486. ", 
+        {text: "R. Aghili, Q. Qin, H. Li, and F. Khomh, “Understanding Web Application Workloads and Their Applications: Systematic Literature Review and Characterization,” in Proceedings of the 40th IEEE International Conference on Software Maintenance and Evolution (ICSME), 2024. ", 
          link: "https://ieeexplore.ieee.org/abstract/document/10795077" },
         {text: "Q. Qin, H. Li, E. Merlo, “Wrangling Data Issues to be Wrangled: Literature Review, Taxonomy, and Industry Case Study,” arXiv preprint, 2024.", 
          link: "https://arxiv.org/abs/2405.16033" },
